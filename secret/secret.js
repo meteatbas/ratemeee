18948dedbf5e984e09f3887f7eb772f622b76687
meteatbs <130202020@kocaeli.edu.tr>
2020-06-29 15:00:49 +0300

    first commit

--- secret/secret.js
+++ secret/secret.js
@@ -8,7 +8,7 @@ module.exports = {
         clientID: '1861759354108916', //Facebook login app id
         clientSecret: 'c88516cf29c19d5d591189d013a7c254', //Facebook login secret key
         profileFields: ['email', 'displayName'],
-        callbackURL: 'http://localhost:3000/auth/facebook/callback',
+        callbackURL: '/auth/facebook/callback',
         passReqToCallback: true
     }
 }
